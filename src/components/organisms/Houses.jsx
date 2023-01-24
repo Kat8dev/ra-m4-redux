149a1fa25d4627428727c1ac4aaad70ac3104d4f
Kevin Mamaqi <kevinmamaqi@gmail.com>
2023-01-24 09:05:05 +0100

    primera-revision

--- src/components/organisms/Houses.jsx
+++ src/components/organisms/Houses.jsx
@@ -24,6 +24,31 @@ function Houses() {
   useEffect(() => {
     const actualState = () => {
       const auxArray = []
+      // Esto de aquí es un error grave
+      // Primero, afecta al rendimiento
+      // Segundo, no es escalable
+      // Tercero, no es mantenible
+      // Cuarto, no es legible
+      // Quinto, no es reutilizable
+      // Sexto, es dificil de testear
+      // Te escribo para que lo veas, pero no lo uses
+      // Ni se te ocurra hacerlo en una pruea técnica de entrevista
+      // Dicho eso vamos a replantearlo para hacerlo adecuadamente (estamos para progresar)
+
+      // Lo primero no filtres en useEFfect, filtra debajo en la vista
+      // Los useEffect por norma suelen ser dificiles de mantener, leer y de testear
+
+      // Cuando filtramos por Ciudad/Typo y posiblemente otros filtros que podemos añadir en un futuro (por ejemplo, por precio)
+      // Lo que hacemos es un filtrado AND, es decir, que se cumplan todas las condiciones
+      // Por ejemplo, si filtramos por Ciudad y Tipo, solo nos devolverá las casas que cumplan ambas condiciones
+      // Si filtramos por Ciudad y Tipo y Precio, solo nos devolverá las casas que cumplan las tres condiciones
+      // Eso significa que tenemos que iterar una vez sobre el array, y en esa misma iteración, comprobar si se cumplen todas las condiciones
+
+      // Te lo planteo más debajo con seudocodigo e intenta sacarlo, sino, nos reunimos y lo vemos:
+      // const casasFiltradas = casas.filter(casa =>
+      //   casa.ciudad === ciudadSeleccionada &&
+      //   casa.tipo === tipoSeleccionado
+      // )
       for (let house in byId) {
         for (let type in byType) {
           if (byId[house].type === byType[type]) {
@@ -48,15 +73,18 @@ function Houses() {
       <Grid gridGap="32px">
         {isLoading && <div>Loading...</div>}
         {isError && <div>Error</div>}
-        {isSucces && currentState.slice(0, perPage).map((id) => (
-          <HouseCard
-            key={byId[id].id}
-            title={byId[id].title}
-            price={`${byId[id].price}€`}
-            img={byId[id].image}
-            link=""
-          />
-        ))}
+        {isSucces &&
+          currentState
+            .slice(0, perPage)
+            .map((id) => (
+              <HouseCard
+                key={byId[id].id}
+                title={byId[id].title}
+                price={`${byId[id].price}€`}
+                img={byId[id].image}
+                link=""
+              />
+            ))}
       </Grid>
       {perPage < allIds.length && (
         <FlexBox align="center">
@@ -73,5 +101,3 @@ function Houses() {
 }
 
 export default styled(Houses)``
-
-
